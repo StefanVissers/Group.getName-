acb1b6ba16a013245e3f6708aecb334a23f86978
ithelost <0908048@hr.nl>
2016-06-22 15:34:57 +0200

    agenda toegevoegd (ThirdActivity)

--- app/src/main/java/getname/group/project_4/SecondActivity.java
+++ app/src/main/java/getname/group/project_4/SecondActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
+import java.util.Calendar;
+
 /**
  * Created by stefan on 21-Jun-16.
  */
@@ -16,7 +18,6 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.secondary_activity);
-
 
         Log.d(msg, " The onCreate() event");
     }
