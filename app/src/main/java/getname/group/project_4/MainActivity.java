625582593b1bd6c8df0b0379c2e56eddaa3c7e6a
0912380 <lucashbp@gmail.com>
2016-06-23 14:21:13 +0200

    Moved changeActivity into ActivityExtender

--- app/src/main/java/getname/group/project_4/MainActivity.java
+++ app/src/main/java/getname/group/project_4/MainActivity.java
@@ -11,33 +11,5 @@ public class MainActivity extends ActivityExtender {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    public void changeActivity(View view) {
-        Intent intent = new Intent();
-        boolean gotDestinationIntent = false;
-
-        String tag = view.getTag().toString();
-        int destination = Integer.parseInt(tag.split(",")[0]);
-
-        if (destination == 1) {
-            gotDestinationIntent = true;
-            intent = new Intent(this, SecondActivity.class);
-        } else if (destination == 2) {
-            gotDestinationIntent = true;
-            intent = new Intent(this, ThirdActivity.class);
-        } else if (destination == 3) {
-            gotDestinationIntent = true;
-            intent = new Intent(this, CalenderActivity.class);
-        }
-
-        if (!gotDestinationIntent) {
-            Log.d("[Main.changeActivity()]", "Didn't get destination intent");
-            intent = new Intent();
-            startActivity(intent);
-        } else {
-            Log.d("[Main.changeActivity()]", "Got destination intent");
-            startActivity(intent);
-        }
-    }
 }
 
