63bde82916c4e55d79bd9c996b802db8ad14b9c6
StefanVissers <0906707@hr.nl>
2016-07-04 15:36:39 +0200

    Merge branch 'master' of C:\Users\stefan\StudioProjects\Group.getName-100 with conflicts.

--- app/src/main/java/getname/group/project_4/activities/MapsActivity.java
+++ app/src/main/java/getname/group/project_4/activities/MapsActivity.java
@@ -3,8 +3,6 @@ package getname.group.project_4.activities;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
-import com.github.mikephil.charting.data.Entry;
-import com.google.android.gms.common.server.converter.StringToIntConverter;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -12,28 +10,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import Data.Queries;
-import Data.builder.ChartData;
 import getname.group.project_4.R;
-import getname.group.project_4.SQL.DatabaseHelper;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
     private GoogleMap mMap;
-    ArrayList<String> labels;
-    String title;
-    String description;
-    ChartData chartData;
-    ArrayList<String> Fietstrommels;
-    List<LatLng> points = new ArrayList<LatLng>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -43,31 +29,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        DatabaseHelper dbhelper = new DatabaseHelper(this);
-        try {
-            dbhelper.createDataBase();
-            dbhelper.openDataBase();
-
-            String xyQuery = Queries.getFietstrommelsCoord()[0].split(": ")[1];
-            ArrayList<Entry> Xentries = dbhelper.getFloatEntryList(xyQuery, 0);
-            ArrayList<Entry> Yentries = dbhelper.getFloatEntryList(xyQuery, 1);
-            ArrayList filter = new ArrayList();
-            filter.add(new Entry(0f, 0));
-
-            for (int i = 0; i < Xentries.size(); i++){
-                LatLng latlong = new LatLng(((double) Xentries.get(i).getVal()), ((double) Yentries.get(i).getVal()));
-                if (!(latlong.latitude == 0f && latlong.longitude == 0f)) {
-                    points.add(latlong);
-                }
-            }
-
-            for (int i = 0; i < points.size(); i++) {
-                mMap.addMarker(new MarkerOptions().position(points.get(i)).title("ik ben een fietstrommel"));
-            }
-        } catch (Exception e) {
-
-        }
-//        LatLng fietstrommel1 = new LatLng(getSql_query());
-
+        LatLng sydney = new LatLng(52, 4.5);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
